59545d4fe5ff499fdaae0cd60dc688290f700aa4
peter.agbanobi <pagbanobi@gmail.com>
2022-11-12 11:45:51 +0000

    update view created for student table. this allow to amend student table

--- application/routes.py
+++ application/routes.py
@@ -5,7 +5,7 @@ from flask import render_template
 from flask_wtf import FlaskForm
 #from flask import flask_wtf,wtforms
 from wtforms import StringField, SubmitField
-from application.forms import NameForm, DelForm, SelectFieldForm, SinForm
+from application.forms import NameForm, DelForm, SelectFieldForm, SinForm, MoodList
 
 #****************FUNCTION****select_List*********************************************
 def select_list(passdata):
@@ -18,6 +18,8 @@ def select_list(passdata):
              return redirect(url_for('register_student'))
         elif passdata.list.data=='Delete A Student':
              return redirect(url_for('delstudent'))
+        elif passdata.list.data=='Update':
+             return redirect(url_for('update'))
         elif passdata.list.data=='Enter Student ID':
              return redirect(url_for('readstudent'))
         elif passdata.list.data=='Home Page':
@@ -121,112 +123,106 @@ def readstudent():
 
 @app.route('/studmod1/<student_id1>', methods=['GET','POST'])
 def readresult(student_id1):
-         #pyoptionform= SelectFieldForm()
+         pyoptionform= SelectFieldForm()
          pystudentform=SinForm()
          if request.method == 'GET':
              studmoodle = moodle.query.filter_by(student_id=student_id1).all()
              pystudent = student.query.filter_by(student_id=student_id1).all()
-            # if request.method =='POST':
-           #  return redirect(url_for('readresult1',stud_id1= student_id1))
-         return render_template('readresult.html', ji_student=pystudent, ji_moodle= studmoodle)
-
-
-@app.route('/studmod2/<stud_id1>', methods=['GET','POST'])
-def readresult1(stud_id1):
-         may= stud_id1
-         pyoptionform= SelectFieldForm()
-         pystudentform=SinForm()
+             return render_template('readresult.html', ji_student=pystudent, ji_moodle= studmoodle, jioptionform= pyoptionform)
          if request.method == 'POST':
-               return redirect(url_for('start'))
-         #return render_template('home.html')
+               return select_list(pyoptionform)
 
 #*****************************************************************************************
 
+#*****************************************************************************************************
 
+ #****************UPDATE ENTRY IN DATABASE***************************************************************
 
-@app.route('/A', methods = ['GET','POST'])
-def read():
+@app.route('/studmodm', methods=['GET','POST'])
+def update():
+        # pyoptionform= SelectFieldForm()
+         pystudentform=SinForm()
+       #  breakpoint()
+         if request.method == 'POST':
+        # WTForms adds the data to the forms we created, then we retrieve it and 
+        # put it into a database object
+          student_id=pystudentform.Stud_id.data
+         # breakpoint()
+          return redirect(url_for('register_student1',student_id1= student_id))
+         return render_template('singlestud.html', jistudentform=pystudentform,)
+
+@app.route('/studmod2/<student_id1>', methods=['GET','POST'])
+def register_student1(student_id1):
     # query list of staff from db
-    #pystaff = Staff.query.join(Subjects).all()
-    pyall_students = student.query.all()
-    # query subjects
-    #pysubjects = Subjects.query.all()
-    pyall_moodle = moodle.query.all()
-    # instantiate empty subject form 
-    # pysubjectform = SubjectList()
-    # # append query to choices
-    # for subj in pysubjects:
-    #     pysubjectform.subjlist.choices.append(
-    #        (subj.id,subj.subject_name)
-    #     )
-
+    pyoptionform= SelectFieldForm()
     # instantiate student input form so that i can use it
     pystudentform = NameForm()
-    pydelstudentform=DelForm()
-    # Grab stuff from the POST
+    if request.method == 'GET':
+         studmoodle = moodle.query.filter_by(student_id=student_id1).all()
+         pystudent = student.query.filter_by(student_id=student_id1).first()
+
+         return render_template('update.html', ji_student=pystudent, ji_moodle= studmoodle, jioptionform= pyoptionform, jistudentform=pystudentform)
+          # Grab stuff from the POST
     if request.method == 'POST':
         # WTForms adds the data to the forms we created, then we retrieve it and 
         # put it into a database object
+        studmoodle = moodle.query.filter_by(student_id=student_id1).all()
+        for k in studmoodle:
+            schooldb.session.delete(k)            
+            schooldb.session.commit()
+        pystudent = student.query.filter_by(student_id=student_id1).first()
+        schooldb.session.delete(pystudent)
+        schooldb.session.commit()
         addstudent = student(student_id=pystudentform.Stud_id.data, surname=pystudentform.SName.data,firstName=pystudentform.FName.data )
         schooldb.session.add(addstudent)
         schooldb.session.commit()
-        #breakpoint()
-        # Send the user back to the frontpage
+        return redirect(url_for('start'))
 
-#*****************************************************************************************************
+                                 # *************
 
-#**************GET SPECIFIC STUDENT MOODLE FROM DATABASE**************************************
+# @app.route('/studmod1/<student_id1>', methods=['GET','POST'])
+# def readresult(student_id1):
+#          pyoptionform= SelectFieldForm()
+#          pystudentform=SinForm()
+#          if request.method == 'GET':
+#              studmoodle = moodle.query.filter_by(student_id=student_id1).all()
+#              pystudent = student.query.filter_by(student_id=student_id1).all()
+#              return render_template('readresult.html', ji_student=pystudent, ji_moodle= studmoodle, jioptionform= pyoptionform)
+#          if request.method == 'POST':
+#                return select_list(pyoptionform)
 
-@app.route('/moodle/<st_id>', methods=["GET"])
-def read_stud_mod(st_id):
-    val1=moodle.query.filter_by(student_id=st_id).all()
-    result =" "
-    for k in range(len(val1)):
-        result+= f"{val1[k].moodle_id} {val1[k].moodleName}\n"
-    return result
-#********************************************************************************************
 
-#*********************GET STUDENTS TAKING A SPECIFIC MOODLE**************************************
+# @app.route('/studmod/', methods=['GET','POST'])
+# def readstudent1():
+#          pymoodle = moodle.query.filter_by(student_id='x0000018').all()
+#          pystudentform = NameForm()
+#          pymoodleform = MoodList()
+#          for mod in pymoodle:
+#               pymoodleform.subjlist.choices.append(
+#                 mod.student_id )
 
-@app.route('/mood/<mod_id>', methods=["GET"])
-def read_mod_stud(mod_id):
-    all_moodle = moodle.query.all()
-    my_string=" "
-    for num in all_moodle:
-         if num.moodle_id==mod_id:
-            my_string += "<br>"+ f"{num.student_id}"
-    return f"student taking this {mod_id} are\n {my_string}"
+#          pyoptionform1= SelectFieldForm()
+#          pystudentform1=SinForm()
+#          if request.method == 'POST':
+#         # WTForms adds the data to the forms we created, then we retrieve it and 
+#         # put it into a database object
+#           student_id=pystudentform1.Stud_id.data
+#          # breakpoint()
+#           #return redirect(url_for('readresult',student_id1= student_id))
+#          return render_template('modupdate.html', jistudentform=pystudentform, jimoodleform=pymoodleform)
 
-#***************************************************************************************************
-@app.route('/studentupdate', methods=["POST"])
-def update(str1,str2,str3):
-    # first_game = Games.query.first()
-    studVal=student(student_id=str1.student_id ,surname= str2.surname,firstName=str3.firstName)
-     #first_game.name = name
-    schooldb.session.add(studVal)
-    schooldb.session.commit()
-    #return first_game.name
- #    
+# @app.route('/studmod1/<student_id1>', methods=['GET','POST'])
+# def readresult2(student_id1):
+#          #pyoptionform= SelectFieldForm()
+#          pystudentform=SinForm()
+#          if request.method == 'GET':
+#              studmoodle = moodle.query.filter_by(student_id=student_id1).all()
+#              pystudent = student.query.filter_by(student_id=student_id1).all()
+#             # if request.method =='POST':
+#            #  return redirect(url_for('readresult1',stud_id1= student_id1))
+#          return render_template('readresult.html', ji_student=pystudent, ji_moodle= studmoodle)
 
-#@app.route('/update/<name>'methods=["POST"])
-# def update(name):
-#     first_game = Games.query.first()
-#     first_game.name = name
-#     crud_db.session.commit()
-#     return first_game.name
- # result= student(student_id=val1.student_id ,surname= val1.surname,firstName=val1.firstName)/database
 
- #***********************************************************************************************************
-
- #****************DELETE ENTRY FROM DATABASE***************************************************************
-
-# @app.route('/<st_id>', methods=["DELETE"])
-# def read_stud_mod(st_id):
-#     val1=moodle.query.filter_by(student_id=st_id).all()
-#     result =" "
-#     for k in range(len(val1)):
-#         result+= f"{val1[k].moodle_id} {val1[k].moodleName}\n"
-#     return result
 # #******************
 
 
